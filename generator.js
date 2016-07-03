7b60c64550659ffafbe6c25c74f6a3fb52d0ca22
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-03 07:20:04 -0400

    docs

--- generator.js
+++ generator.js
@@ -6,6 +6,16 @@ var isValid = require('is-valid-app');
 module.exports = function(app) {
   if (!isValid(app, 'generate-example')) return;
 
+  /**
+   * Generate an `example.txt` file in the current working directory.
+   *
+   * ```sh
+   * $ gen example
+   * ```
+   * @name example
+   * @api public
+   */
+
   app.task('example', function(cb) {
     return app.src('example.txt', {cwd: path.resolve(__dirname, 'templates')})
       .pipe(app.dest(app.options.dest || app.cwd));
